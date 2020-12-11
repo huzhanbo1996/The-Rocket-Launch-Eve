b49086b6afffeb3d622cf3525ac79f00501a555c
ZHANBO HU <1072505702@qq.com>
2020-12-11 19:22:36 +0800

    add persist of sprite, adapte phone resolution

--- Assets/Scripts/SpritesPath.cs
+++ Assets/Scripts/SpritesPath.cs
@@ -7,5 +7,16 @@ using System.Collections.Generic;
 public class SpritesPath : MonoBehaviour
 {
     // public Dictionary<string, Sprite> PathVSSprite = new Dictionary<string, Sprite>();
+    [SerializeField] public List<Sprite> AllSp = new List<Sprite>();
+    [SerializeField] public List<string> AllSpName = new List<string>();
     public Dictionary<Sprite, string> SpriteVSPath = new Dictionary<Sprite, string>();
+
+    private void Start()
+    {
+        SpriteVSPath.Clear();
+        for (int idx = 0; idx < AllSp.Count;idx++)
+        {
+            SpriteVSPath.Add(AllSp[idx], AllSpName[idx]);
+        }
+    }
 }
--- Assets/Scripts/SpritesPathInspector.cs
+++ Assets/Scripts/SpritesPathInspector.cs
@@ -12,42 +12,48 @@ public class SpritesPathInspector : Editor
     void OnEnable()
     {
         var spritesPath = (SpritesPath)target;
-        if (spritesPath.SpriteVSPath.Count != 0 ) return;
-        spritesPath.SpriteVSPath.Clear();
+        if (spritesPath.AllSp.Count != 0 ) return;
+        spritesPath.AllSp.Clear();
+        spritesPath.AllSpName.Clear();
         // spritesPath.PathVSSprite.Clear();
 
         var allSp = Resources.FindObjectsOfTypeAll<Sprite>();
-        foreach(var sp in allSp)
+        foreach (var sp in allSp)
         {
-            spritesPath.SpriteVSPath.Add(sp, GetResourcePath(sp));
+            spritesPath.AllSp.Add(sp);
+            spritesPath.AllSpName.Add(GetResourcePath(sp));
             // spritesPath.PathVSSprite.Add(GetResourcePath(sp), sp);
         }
 
         var allSceneObj = Resources.FindObjectsOfTypeAll<SceneObj>();
-        foreach(var obj in allSceneObj)
+        foreach (var obj in allSceneObj)
         {
             if (obj.picIdle == null || spritesPath.SpriteVSPath.ContainsKey(obj.picIdle)) continue;
-            spritesPath.SpriteVSPath.Add(obj.picIdle, GetResourcePath(obj.picIdle));
+            spritesPath.AllSp.Add(obj.picIdle);
+            spritesPath.AllSpName.Add(GetResourcePath(obj.picIdle));
             // spritesPath.PathVSSprite.Add(GetResourcePath(sp), sp);
         }
 
         var quizWire = Resources.FindObjectsOfTypeAll<QuizWire>()[0];
-        foreach(var sp in quizWire.mSpVSPic.Values)
+        foreach (var sp in quizWire.mSpVSPic.Values)
         {
             if (sp == null || spritesPath.SpriteVSPath.ContainsKey(sp)) continue;
-            spritesPath.SpriteVSPath.Add(sp, GetResourcePath(sp));
+            spritesPath.AllSp.Add(sp);
+            spritesPath.AllSpName.Add(GetResourcePath(sp));
         }
 
         var quiz4NPC = Resources.FindObjectsOfTypeAll<Quiz4NPC>()[0];
         if (quiz4NPC.mPictureHold != null && !spritesPath.SpriteVSPath.ContainsKey(quiz4NPC.mPictureHold))
         {
-            spritesPath.SpriteVSPath.Add(quiz4NPC.mPictureHold, GetResourcePath(quiz4NPC.mPictureHold));
+            spritesPath.AllSp.Add(quiz4NPC.mPictureHold);
+            spritesPath.AllSpName.Add(GetResourcePath(quiz4NPC.mPictureHold));
         }
 
         var quizMirror = Resources.FindObjectsOfTypeAll<QuizMirror>()[0];
         if (quizMirror.mPic != null && !spritesPath.SpriteVSPath.ContainsKey(quizMirror.mPic))
         {
-            spritesPath.SpriteVSPath.Add(quizMirror.mPic, GetResourcePath(quizMirror.mPic));
+            spritesPath.AllSp.Add(quizMirror.mPic);
+spritesPath.AllSpName.Add( GetResourcePath(quizMirror.mPic));
         }
 
         foreach(var p in spritesPath.SpriteVSPath)
